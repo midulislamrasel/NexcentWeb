b2cf6db288090909de81ccb3f8ae30772b409ace
midulislamrasel <midulraddy121@gmail.com>
2023-11-19 18:37:22 +0600

    Add

--- src/components/marketing/Marketing.jsx
+++ src/components/marketing/Marketing.jsx
@@ -14,69 +14,63 @@ function Marketing() {
         more.​
       </p>
       <div className={Styles.marketing_section}>
-        <div>
-          <div className={Styles.markting_Img_card}>
-            <Image
-              className={Styles.images_style}
-              src={markting_Img}
-              alt="markting_Img"
-            />
-            <div className={Styles.card}>
-              <div className={Styles.abced}>
-                <p>Creating Streamlined Safeguarding Processes with OneRen</p>
-                <ButtonOutLine
-                  style={{ fontSize: "20px" }}
-                  label={"Readmore"}
-                  type={"button"}
-                  background="none"
-                  padding={"5px 10px"}
-                />
-              </div>
+        <div className={Styles.markting_Img_card}>
+          <Image
+            className={Styles.images_style}
+            src={markting_Img}
+            alt="markting_Img"
+          />
+          <div className={Styles.card}>
+            <div className={Styles.abced}>
+              <p>Creating Streamlined Safeguarding Processes with OneRen</p>
+              <ButtonOutLine
+                style={{ fontSize: "20px" }}
+                label={"Readmore"}
+                type={"button"}
+                background="none"
+                padding={"5px 10px"}
+              />
             </div>
           </div>
         </div>
 
         {/* === */}
-        <div>
-          <div className={Styles.markting_Img_card}>
-            <Image
-              className={Styles.images_style}
-              src={markting_Img2}
-              alt="markting_Img"
-            />
-            <div className={Styles.card}>
-              <div className={Styles.abced}>
-                <p>
-                  What are your safeguarding responsibilities and how can you
-                  manage them?
-                </p>
-                <ButtonOutLine
-                  label={"Readmore"}
-                  type={"button"}
-                  background="none"
-                  padding={"5px 10px"}
-                />
-              </div>
+        <div className={Styles.markting_Img_card}>
+          <Image
+            className={Styles.images_style}
+            src={markting_Img2}
+            alt="markting_Img"
+          />
+          <div className={Styles.card}>
+            <div className={Styles.abced}>
+              <p>
+                What are your safeguarding responsibilities and how can you
+                manage them?
+              </p>
+              <ButtonOutLine
+                label={"Readmore"}
+                type={"button"}
+                background="none"
+                padding={"5px 10px"}
+              />
             </div>
           </div>
         </div>
-        <div>
-          <div className={Styles.markting_Img_card}>
-            <Image
-              className={Styles.images_style}
-              src={markting_Img3}
-              alt="markting_Img"
-            />
-            <div className={Styles.card}>
-              <div className={Styles.abced}>
-                <p>Revamping the Membership Model with Triathlon Australia</p>
-                <ButtonOutLine
-                  label={"Readmore"}
-                  type={"button"}
-                  background="none"
-                  padding={"5px 10px"}
-                />
-              </div>
+        <div className={Styles.markting_Img_card}>
+          <Image
+            className={Styles.images_style}
+            src={markting_Img3}
+            alt="markting_Img"
+          />
+          <div className={Styles.card}>
+            <div className={Styles.abced}>
+              <p>Revamping the Membership Model with Triathlon Australia</p>
+              <ButtonOutLine
+                label={"Readmore"}
+                type={"button"}
+                background="none"
+                padding={"5px 10px"}
+              />
             </div>
           </div>
         </div>
